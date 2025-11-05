13fe31dc3f72a86ed469c8a5777f3db8050c92f9
Loc Blue <loctran0169@gmail.com>
2025-11-05 11:37:25 +0700

    finish splash UI

--- app/src/main/java/chin/pswm/gps/photo/location/map/activity/first_open/ui/splash/view/SplashContent.kt
+++ app/src/main/java/chin/pswm/gps/photo/location/map/activity/first_open/ui/splash/view/SplashContent.kt
@@ -1,88 +1,62 @@
 package chin.pswm.gps.photo.location.map.activity.first_open.ui.splash.view
 
-import android.os.Build.VERSION.SDK_INT
-import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import chin.pswm.gps.photo.location.map.earthview.custom.AppImage
+import chin.pswm.gps.photo.location.map.earthview.custom.CenterBox
+import chin.pswm.gps.photo.location.map.earthview.custom.CenterColumn
+import chin.pswm.gps.photo.location.map.ui.theme.appFont
+import chin.pswm.gps.photo.location.map.ui.theme.colorWhite
+import chin.pswm.gps.photo.location.map_debug.R
 
 @Composable
 fun SplashContent() {
-    val context = LocalContext.current
 
-//    CenterBox(Modifier.fillMaxSize()) {
-//        AppImage(
-//            res = R.drawable.bg_splash,
-//            contentScale = ContentScale.FillBounds,
-//            modifier = Modifier.fillMaxSize()
-//        )
-//
-//        ConstraintLayout(
-//            modifier = Modifier
-//                .fillMaxSize()
-//        ) {
-//
-//            val (image, text, gif) = createRefs()
-//
-//            AppImage(
-//                res = R.drawable.img_logo,
-//                contentScale = ContentScale.FillWidth,
-//                modifier = Modifier
-//                    .constrainAs(image) {
-//                        bottom.linkTo(text.top)
-//                        centerHorizontallyTo(parent)
-//                    }
-//                    .fillMaxWidth(280f / 720)
-//            )
-//
-//            Text(
-//                stringResource(R.string.app_name),
-//                style = appFont(800, 28),
-//                color = Color(0xFF0E66FF),
-//                textAlign = TextAlign.Center,
-//                modifier = Modifier
-//                    .constrainAs(text) {
-//                        bottom.linkTo(gif.top)
-//                        centerHorizontallyTo(parent)
-//                    }
-//                    .padding(top = 16.dp)
-//            )
-//
-//            val gifEnabledLoader = remember {
-//                ImageLoader.Builder(context)
-//                    .components {
-//                        if (SDK_INT >= 28) {
-//                            add(ImageDecoderDecoder.Factory())
-//                        } else {
-//                            add(GifDecoder.Factory())
-//                        }
-//                    }.build()
-//            }
-//
-//            AsyncImage(
-//                model = R.drawable.gif_splash,
-//                imageLoader = gifEnabledLoader,
-//                contentDescription = null,
-//                modifier = Modifier
-//                    .constrainAs(gif) {
-//                        centerTo(parent)
-//                    }
-//                    .fillMaxWidth(0.1625f)
-//                    .aspectRatio(1f)
-//            )
-//        }
-//    }
+    CenterBox(Modifier.fillMaxSize()) {
+        AppImage(
+            res = R.drawable.splash,
+            contentScale = ContentScale.FillBounds,
+            modifier = Modifier.fillMaxSize()
+        )
+
+        CenterColumn(Modifier.fillMaxSize()) {
+
+            AppImage(
+                res = R.drawable.splash_logo,
+                contentScale = ContentScale.FillWidth,
+                modifier = Modifier.fillMaxWidth(0.38f)
+            )
+
+            Text(
+                stringResource(R.string.app_name),
+                style = appFont(600, 22),
+                color = Color.White,
+                textAlign = TextAlign.Center,
+                modifier = Modifier
+                    .padding(top = 20.dp)
+                    .padding(horizontal = 24.dp)
+            )
+        }
+
+        CircularProgressIndicator(
+            color = colorWhite,
+            modifier = Modifier
+                .padding(24.dp)
+                .align(Alignment.BottomCenter)
+        )
+    }
 }
 
 @Preview
